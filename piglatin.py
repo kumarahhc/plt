ebcd6aafbbdc1791f27e7160a84c6b9d668dbd4c
i3cubes <47766255+i3cubes@users.noreply.github.com>
2024-10-31 17:26:29 +0200

    US-2+3

--- piglatin.py
+++ piglatin.py
@@ -1,12 +1,18 @@
 
 class PigLatin:
-
     def __init__(self, phrase: str):
-        self.phrase = phrase
+        self.phrase = phrase if phrase else "nil"
 
     def get_phrase(self) -> str:
         return self.phrase
 
     def translate(self) -> str:
-        pass
+        if self.phrase == "nil":
+            return "nil"
+        elif self.phrase.endswith('y'):
+            return self.phrase + 'nqy'
+        elif self.phrase[-1] in 'aeiou':
+            return self.phrase + 'yay'
+        else:
+            return self.phrase
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -4,9 +4,33 @@ from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
-
+    #US-1
     def test_input_phase(self):
         translater= PigLatin("hellow world")
         self.assertEqual("hellow world",translater.get_phrase())
 
+    #US-2
+    def test_empty_prase(self):
+        translater= PigLatin("")
+        self.assertEqual("nil",translater.translate())
 
+    # US-3
+    def test_word_end_with_y(self):
+        translater = PigLatin("why")
+        self.assertEqual("whynqy", translater.translate())
+
+    def test_word_end_with_vovel_a(self):
+        translater = PigLatin("salsa")
+        self.assertEqual("salsayay", translater.translate())
+    def test_word_end_with_volel_e(self):
+        translater = PigLatin("salse")
+        self.assertEqual("salseyay", translater.translate())
+    def test_word_end_with_volel_i(self):
+        translater = PigLatin("salsi")
+        self.assertEqual("salsiyay", translater.translate())
+    def test_word_end_with_volel_o(self):
+        translater = PigLatin("salso")
+        self.assertEqual("salsoyay", translater.translate())
+    def test_word_end_with_volel_u(self):
+        translater = PigLatin("salsu")
+        self.assertEqual("salsuyay", translater.translate())
